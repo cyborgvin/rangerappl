2597834d55c3c9b63338e4e93117af863c9ae66c
cyborgvin <91885990+cyborgvin@users.noreply.github.com>
2022-04-04 16:53:40 +0100

    Update Camera.jsx

--- src/components/Camera/Camera.jsx
+++ src/components/Camera/Camera.jsx
@@ -5,67 +5,31 @@ import { useEffect } from 'react';
 
 const Camera = () => {
 
-    let videoRef = useRef(null);
-    let photoRef = useRef(null)
-  
-    const getVideo = () => {
-      navigator.mediaDevices
-        .getUserMedia({
-          video: true
-        })
-        .then((stream) => {
-          let video = videoRef.current;
-          video.srcObject = stream;
-          video.play();
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    };
-  
-    const takePicture = () => {
-      const width = 500
-      const height = width / (16 / 9)
-      
-      let video = videoRef.current
-      let photo = photoRef.current
-  
-      photo.width = width
-      photo.height = height
-  
-      let ctx = photo.getContext('2d')
-  
-      ctx.drawImage(video, 0, 0, width, height)
-      
-    }
-  
-    const clearImage = () => {
-      let photo = photoRef.current
-      let ctx = photo.getContext('2d')
-  
-      ctx.clearRect(0,0,photo.width,photo.height)
-    }
-  
-    useEffect(() => {
-      getVideo();
-    }, [videoRef]);
-  
-    return (
-        <div className="container">
+  const webcamRef = React.useRef(null);
+  const [imgSrc, setImgSrc] = React.useState(null);
 
-        <canvas className="container" ref={photoRef}></canvas>
-  
-        <button onClick={clearImage} className="btn btn-primary container">Clear Image</button>
-  
-        <video ref={videoRef} className="container"></video>
-  
-        <button onClick={takePicture} className="btn btn-danger container">Take Picture</button>
-  
-        
-  
-        <br/><br/>
-      </div>
-    );
-}
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
 
-export default Camera;
+  return (
+    <>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+      />
+      <button onClick={capture}>Capture photo</button>
+      {imgSrc && (
+        <img
+          src={imgSrc}
+        />
+      )}
+    </>
+  );
+};
+
+ReactDOM.render(<Camera />, document.getElementById("root"));
+
+// https://www.npmjs.com/package/react-webcam
